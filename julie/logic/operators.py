fc1357da6e90ed2c19e8d87e98cb1f558d3daf64
MarcelloBB <marcellobelanda@gmail.com>
2021-08-21 16:52:10 -0300

    add operators module and the tests module

---
+++ julie/logic/operators.py
@@ -0,0 +1,5 @@
+def OR(a, b):
+    return a or b
+
+def AND(a, b):
+    return a and b
---
+++ julie/logic/tests.py
@@ -0,0 +1,9 @@
+import operators
+
+def TEST_():
+    """
+    [FUNC] TEST_:
+    Silly tests
+    """
+
+    return operators.OR(True, False), operators.AND(True, False)
