63e59884df6ed1a03ef3445e3995f8f90c7127cb
MarcelloBB <marcellobelanda@gmail.com>
2021-08-21 22:13:08 -0300

    add math module

---
+++ julie/math/equations.py
@@ -0,0 +1,20 @@
+"""class Equation:
+    def __init__(self, a, b, c):
+        self.a, self.b = a, b;
+
+    def solve(self):
+        # Case 1:
+        if:
+            pass
+
+
+        # Case 2:
+        elif:
+            pass
+
+
+        # Case 3:
+        elif:
+            pass
+
+        """
