faac5fb93999326315180588dd38f51131ad32e3
MarcelloBB <marcellobelanda@gmail.com>
2021-08-23 12:07:19 -0300

    UPDATE CONSTS: ADD NEW FEATURES -> (golden, deg, yr etc)

--- julie/consts/consts.py
+++ julie/consts/consts.py
@@ -3,9 +3,21 @@ import math
 class Consts:
     """
     [CLASS] Consts: 
-    This class contains some very import math/physics constants
+    This class contains some very import constants.
     """
     
+    # MATH
     PI            = math.pi
     E             = math.e
-    EARTH_GRAVITY = 10
+    GOLDEN        = (1 + math.sqrt(5)) / 2
+
+    # ANGLE -> rad
+    DEGREE        = math.pi / 180
+
+    # TIME -> sec
+    MINUTE        = 60
+    HOUR          = 60 * MINUTE
+    DAY           = 24 * HOUR
+    WEEK          = 7 * DAY
+    YEAR          = 365 * DAY
+
